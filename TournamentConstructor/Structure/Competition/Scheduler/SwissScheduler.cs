38c632a810494056630171c0afcdc1af1338ea86
A1essandro <alexander.yermakov.1990@gmail.com>
2017-06-28 00:30:53 +0400

    Add SwissScheduler

---
+++ TournamentConstructor/Structure/Competition/Scheduler/SwissScheduler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TournamentConstructor.Structure.Competition.Scheduler
+{
+    public class SwissScheduler : IScheduler
+    {
+
+        public ushort ToursCount { get; private set; }
+
+        private ushort _players;
+
+        public SwissScheduler(ushort players, ushort tours)
+        {
+            ToursCount = tours;
+            _players = players;
+        } 
+
+        public IEnumerable<IEnumerable<Tuple<int, int>>> GetTours()
+        {
+            for(var i = 0; i < ToursCount; i++)
+            {
+                yield return _getTour();
+            }
+        }
+
+        private IEnumerable<Tuple<int, int>> _getTour()
+        {
+            var result = new Tuple<int, int>[_players / 2];
+            for(var i = 0; i < _players / 2; i+=2)
+            {
+                result[i] = new Tuple<int, int>(i, i + 1);
+            }
+            return result;
+        }
+
+    }
+}
